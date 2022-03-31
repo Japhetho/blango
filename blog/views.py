51aaefae8b62b67cc71690e45e73fdafc666a75b
Japheth Nyandoro <nyandorojapheth@gmail.com>
2022-03-31 14:04:33 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,11 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
-  return render(request, "blog/index.html", {"posts": posts})
+    from django.http import HttpResponse
+    return HttpResponse(str(request.user).encode("ascii"))
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
   post = get_object_or_404(Post, slug=slug)
